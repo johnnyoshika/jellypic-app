7effd7fff60101e11fc9cf3e37a4f0e0f4ffbba5
Johnny Oshika <johnnyoshika@gmail.com>
2020-02-05 08:22:16 -0800

    Add Login component

---
+++ src/components/Login/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import LoginForm from './LoginForm';
+
+const LOGIN_USER = gql`
+  mutation login($accessToken: String!) {
+    login(input: { accessToken: $accessToken }) {
+      me {
+        id
+      }
+    }
+  }
+`;
+
+const Login = () => {
+  const client = useApolloClient();
+  const [login, { loading, error }] = useMutation(LOGIN_USER, {
+    onCompleted({ login }) {
+      localStorage.setItem('loggedInUserId', login.me.id);
+      client
+        .resetStore()
+        .then(() => client.writeData({ data: { isLoggedIn: true } }));
+    },
+  });
+
+  return <LoginForm login={login} loading={loading} error={error} />;
+};
+
+export default Login;

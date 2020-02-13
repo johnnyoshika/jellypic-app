ecddc79da84b70dd454955d1cf030ed874b75427
Johnny Oshika <johnnyoshika@gmail.com>
2020-02-13 08:32:47 -0800

    Load posts in Home

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import Error from 'components/Error';
+import Loading from 'components/Loading';
+import { GET_POSTS } from 'schema/queries';
+import Card from 'components/Card';
 
 import './style.css';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, loading, error } = useQuery(GET_POSTS);
+
+  if (error) return <Error error={error}></Error>;
+
+  if (loading && !data) return <Loading />;
+
+  return (
+    <div className="home-container">
+      <div className="gutter" />
+      <div className="home-main">
+        {data.posts.nodes.map(post => (
+          <Card key={post.id} post={post} />
+        ))}
+      </div>
+      <div className="gutter" />
+    </div>
+  );
 };
 
 export default Home;

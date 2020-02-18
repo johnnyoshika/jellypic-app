e5216f2c36363bbd98bfe211bd92ef46ce617e04
Johnny Oshika <johnnyoshika@gmail.com>
2020-02-17 22:16:11 -0800

    Add Post page

--- src/routes/Post/index.js
+++ src/routes/Post/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_POST } from 'schema/queries';
+import Card from 'components/Card';
+import Error from 'components/Error';
+import Loading from 'components/Loading';
 
 import './style.css';
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({
+  match: {
+    params: { id },
+  },
+}) => {
+  const { data, loading, error } = useQuery(GET_POST, {
+    variables: {
+      id,
+    },
+  });
+
+  if (error) return <Error error={error} />;
+
+  if (loading && !data) return <Loading />;
+
+  const { post } = data;
+  return (
+    <div className="post-container">
+      <div className="gutter" />
+      <div className="post-main">
+        <Card key={post.id} post={post} />
+      </div>
+      <div className="gutter" />
+    </div>
+  );
 };
 
 export default Post;

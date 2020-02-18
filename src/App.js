92abbeb9fd0020288230601c8adba5228dcfca49
Johnny Oshika <johnnyoshika@gmail.com>
2020-02-17 18:33:49 -0800

    Install and configure React Toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import { useMe } from 'context/user-context';
@@ -6,6 +7,12 @@ import Loading from 'components/Loading';
 import Login from 'components/Login';
 import Session from 'components/Session';
 import Dashboard from 'routes';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+
+toast.configure({
+  autoClose: 6000,
+});
 
 const IS_LOGGED_IN = gql`
   query isUserLoggedIn {

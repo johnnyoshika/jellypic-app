d9135f2f91f7b60e83aa9356eb181ea1cda6bba1
Johnny Oshika <johnnyoshika@gmail.com>
2020-02-19 07:31:26 -0800

    Immediately load post page from cache if exists

--- src/routes/Post/index.js
+++ src/routes/Post/index.js
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/react-hooks';
 import { GET_POST } from 'schema/queries';
 import Card from 'components/Card';
 import Error from 'components/Error';
-import Loading from 'components/Loading';
+import Preview from './Preview';
 
 import './style.css';
 
@@ -20,14 +20,15 @@ const Post = ({
 
   if (error) return <Error error={error} />;
 
-  if (loading && !data) return <Loading />;
-
-  const { post } = data;
   return (
     <div className="post-container">
       <div className="gutter" />
       <div className="post-main">
-        <Card key={post.id} post={post} />
+        {loading ? (
+          <Preview id={id} />
+        ) : (
+          <Card key={data.post.id} post={data.post} />
+        )}
       </div>
       <div className="gutter" />
     </div>

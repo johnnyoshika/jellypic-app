78613b86014ad4f0f876a567aa75da9d6d87e74e
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-09 15:00:06 -0700

    Add Service Worker (sw.js) using Workbox

---
+++ src/sw.js
@@ -0,0 +1,85 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js',
+);
+
+var version = 'v1';
+
+self.addEventListener('activate', e => {
+  // Claim clients that aren't already under its control.
+  // Useful for new sw installation, as the page that registered the service worker for the first time won't be controlled until it's loaded again.
+  self.clients.claim();
+});
+
+self.addEventListener('message', e => {
+  if (!e.data) return;
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      break;
+  }
+});
+
+workbox.core.setCacheNameDetails({
+  suffix: version,
+});
+
+// Workbox will populate this list (as defined in workbox-config.js) during build.
+workbox.precaching.precacheAndRoute([]);
+
+// Google font css and woff2 files
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googlefonts',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 5,
+      }),
+    ],
+  }),
+);
+
+// Font Awesome css and woff2 files
+workbox.routing.registerRoute(
+  new RegExp('https://maxcdn.bootstrapcdn.com/font-awesome/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'fontawesome',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+      }),
+    ],
+  }),
+);
+
+// Cloudinary images. We won't cache cloudinary widget script (from widget.cloudinary.com) for uploading images b/c it doesn't support CORS.
+workbox.routing.registerRoute(
+  new RegExp('https://res.cloudinary.com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'cloudinary',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 200,
+        // maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days (perhaps a bug in workbox? the moment i set maxAgentSeconds, it no longer finds resource in cache)
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'static-resources',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+// make sure 'index.html' is pre-cached, otherwise we'll see a 'an object that was not a Response was passed to respondWith()' error in Console and page won't load.
+workbox.routing.registerNavigationRoute('/index.html');

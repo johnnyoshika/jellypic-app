010f56395f7c25fb5cb8c594191532d01e82d24d
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-09 22:36:13 -0700

    Add Refresher component for when Service Worker updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import {
   NavLink,
 } from 'react-router-dom';
 import { useMe } from 'context/user-context';
+import Refresher from 'components/Refresher';
 import Uploader from 'components/Uploader';
 import Home from './Home';
 import Post from './Post';
@@ -39,70 +40,73 @@ const Dashboard = () => {
   };
 
   return (
-    <Router>
-      <div className="page">
-        <div className="header">
-          <div className="gutter" />
-          <div className="header-content">
-            <div className="font-lobster">
-              <Link to="/">Jellypic</Link>
+    <>
+      <Refresher />
+      <Router>
+        <div className="page">
+          <div className="header">
+            <div className="gutter" />
+            <div className="header-content">
+              <div className="font-lobster">
+                <Link to="/">Jellypic</Link>
+              </div>
+              <div className="header-content-icons text-right">
+                <div />
+                <div>
+                  <NavLink to="/" exact activeClassName="nav-active">
+                    <i
+                      className="fa fa-home fa-2x"
+                      aria-hidden="true"
+                    />
+                  </NavLink>
+                </div>
+                <div>
+                  <Uploader
+                    setUploadState={setUploadState}
+                    setUploadError={setUploadError}
+                  >
+                    <i
+                      className="fa fa-cloud-upload fa-2x"
+                      aria-hidden="true"
+                    />
+                  </Uploader>
+                </div>
+                <div>
+                  <NavLink
+                    to="/subscription"
+                    activeClassName="nav-active"
+                  >
+                    <i
+                      className="fa fa-bell fa-2x"
+                      aria-hidden="true"
+                    />
+                  </NavLink>
+                </div>
+                <div>
+                  <NavLink
+                    to={'/users/' + me.id}
+                    activeClassName="nav-active"
+                  >
+                    <i
+                      className="fa fa-user fa-2x"
+                      aria-hidden="true"
+                    />
+                  </NavLink>
+                </div>
+              </div>
             </div>
-            <div className="header-content-icons text-right">
-              <div />
-              <div>
-                <NavLink to="/" exact activeClassName="nav-active">
-                  <i
-                    className="fa fa-home fa-2x"
-                    aria-hidden="true"
-                  />
-                </NavLink>
-              </div>
-              <div>
-                <Uploader
-                  setUploadState={setUploadState}
-                  setUploadError={setUploadError}
-                >
-                  <i
-                    className="fa fa-cloud-upload fa-2x"
-                    aria-hidden="true"
-                  />
-                </Uploader>
-              </div>
-              <div>
-                <NavLink
-                  to="/subscription"
-                  activeClassName="nav-active"
-                >
-                  <i
-                    className="fa fa-bell fa-2x"
-                    aria-hidden="true"
-                  />
-                </NavLink>
-              </div>
-              <div>
-                <NavLink
-                  to={'/users/' + me.id}
-                  activeClassName="nav-active"
-                >
-                  <i
-                    className="fa fa-user fa-2x"
-                    aria-hidden="true"
-                  />
-                </NavLink>
-              </div>
-            </div>
+            <div className="gutter" />
           </div>
-          <div className="gutter" />
+          {renderUploaderStatus()}
+          <>
+            <Route exact path="/" component={Home} />
+            <Route path="/posts/:id" component={Post} />
+            <Route path="/subscription" component={Subscription} />
+            <Route path="/users/:id" component={User} />
+          </>
         </div>
-        {renderUploaderStatus()}
-        <>
-          <Route exact path="/" component={Home} />
-          <Route path="/posts/:id" component={Post} />
-          <Route path="/subscription" component={Subscription} />
-          <Route path="/users/:id" component={User} />
-        </>
-      </div>
-    </Router>
+      </Router>
+    </>
   );
 };
 

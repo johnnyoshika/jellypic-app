914a82bb7e87637bf297c433eecd1b96dd85fda3
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-19 12:07:21 -0700

    Remove Facebook login

--- src/components/Login/LoginForm/index.js
+++ src/components/Login/LoginForm/index.js
@@ -1,64 +1,14 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Error from 'components/Error';
 
 import './style.css';
 
-const ensureFacebookSdkLoaded = callback => {
-  if (window.fbAsyncInit) return callback();
-
-  window.fbAsyncInit = () => {
-    window.FB.init({
-      appId: process.env.REACT_APP_FACEBOOK_APP_ID,
-      cookie: false,
-      xfbml: false,
-      version: 'v6.0',
-    });
-    callback();
-  };
-
-  (function(d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-      return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = '//connect.facebook.net/en_US/sdk.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, 'script', 'facebook-jssdk');
-};
-
 const LoginForm = ({ login, loading, error }) => {
   const [checking, setChecking] = useState(true);
 
-  const memoizedCallback = useCallback(
-    accessToken => {
-      login({
-        variables: {
-          accessToken,
-        },
-      }).catch(() => setChecking(false)); // Unless we catch, a network error will cause an unhandled rejection: https://github.com/apollographql/apollo-client/issues/3963
-    },
-    [login],
-  );
+  useEffect(() => {}, []);
 
-  useEffect(() => {
-    ensureFacebookSdkLoaded(() => {
-      window.FB.getLoginStatus(response => {
-        if (response.status === 'connected')
-          memoizedCallback(response.authResponse.accessToken);
-        else setChecking(false);
-      });
-    });
-  }, [memoizedCallback]);
-
-  const loginWithFacebook = () => {
-    window.FB.login(response => {
-      if (response.status === 'connected')
-        memoizedCallback(response.authResponse.accessToken);
-    });
-  };
+  const handleLoginClick = () => {};
 
   return (
     <div className="login-container">
@@ -69,9 +19,9 @@ const LoginForm = ({ login, loading, error }) => {
           <button
             className="btn btn-primary btn-lg"
             disabled={checking || loading}
-            onClick={() => loginWithFacebook()}
+            onClick={() => handleLoginClick()}
           >
-            Log in with Facebook
+            Log in
           </button>
           {error && <Error error={error} />}
         </div>

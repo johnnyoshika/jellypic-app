255c9ae02971f998404a1888144769cdf4d90b8b
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-20 10:29:47 -0700

    Login with Auth0

--- src/components/Login/LoginForm/index.js
+++ src/components/Login/LoginForm/index.js
@@ -1,14 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { signIn, checkSession } from '../Auth';
 import Error from 'components/Error';
 
 import './style.css';
 
 const LoginForm = ({ login, loading, error }) => {
+  const [loginErrorMessage, setLoginErrorMessage] = useState(null);
   const [checking, setChecking] = useState(true);
 
-  useEffect(() => {}, []);
+  const memoizedCallback = useCallback(
+    token => {
+      login({
+        variables: {
+          token,
+        },
+      }).catch(() => setChecking(false)); // Unless we catch, a network error will cause an unhandled rejection: https://github.com/apollographql/apollo-client/issues/3963
+    },
+    [login],
+  );
 
-  const handleLoginClick = () => {};
+  useEffect(() => {
+    checkSession().then(
+      authResult => {
+        memoizedCallback(authResult.idToken);
+      },
+      err => {
+        if (err.code === 'login_required') signIn();
+        else {
+          setLoginErrorMessage(err.description);
+          setChecking(false);
+        }
+      },
+    );
+  }, [memoizedCallback]);
+
+  const handleLoginClick = () => {
+    setChecking(true);
+    signIn();
+  };
 
   return (
     <div className="login-container">
@@ -24,6 +53,7 @@ const LoginForm = ({ login, loading, error }) => {
             Log in
           </button>
           {error && <Error error={error} />}
+          {loginErrorMessage && <Error error={loginErrorMessage} />}
         </div>
       </div>
       <div className="gutter" />

1675474e3968be00f7a31cd24428eab9bd12f793
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-23 10:01:34 -0700

    Purge 'apollo-cache-persist' localStorage so that we don't leave our app hanging

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { useApolloClient } from '@apollo/react-hooks';
 import useAuth0 from 'hooks/useAuth0';
+import { CachePersistor } from 'apollo-cache-persist';
 
 const Logout = () => {
   const client = useApolloClient();
   const { signOut } = useAuth0();
+  const persistor = new CachePersistor({
+    cache: client.cache,
+    storage: window.localStorage,
+  });
 
   const handleClick = e => {
     e.preventDefault();
     client.clearStore().then(() => {
       localStorage.removeItem('auth-token');
-      signOut();
+      persistor.purge().then(
+        () => signOut(),
+        () => {
+          // Wipe out 'apollo-cache-persist' as a last resort b/c clearStore assigns {} to 'apollo-cache-persist' localStorage
+          // and leave our cache in a unusable state: https://github.com/apollographql/apollo-cache-persist/issues/126#issuecomment-602725578
+          // Apollo 3.x will fix this: https://github.com/apollographql/apollo-cache-persist/issues/126#issuecomment-602723222
+          localStorage.removeItem('apollo-cache-persist');
+          signOut();
+        },
+      );
     });
   };
 
